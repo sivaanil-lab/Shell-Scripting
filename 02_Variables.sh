e372c3ceafbf3f267cf216be46b4c2d239fff3b4
sivaanil-lab <timmisettyshiva@gmail.com>
2026-03-11 23:52:19 +0530

    variables added

---
+++ 02_Variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+Person1=Shiva
+Person2=Sathi
+
+echo "$Person1 :: Hey, Do you have a minute!"
+echo "$Person2 :: Hi $Person1, Yeah Wssup?"
+echo "$Person1 :: Don't want to brag it but, I think no one has stuck in my head like you did and I like it for some reason."
