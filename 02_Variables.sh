d78bfc234c607efd0ed1edaa39d4489322b2270a
sivaanil-lab <timmisettyshiva@gmail.com>
2026-03-12 00:43:18 +0530

    variables added

--- 02_Variables.sh
+++ 02_Variables.sh
@@ -5,4 +5,4 @@ Person2=Sathi
 
 echo "$Person1 :: Hey, Do you have a minute!"
 echo "$Person2 :: Hi $Person1, Yeah Wssup?"
-echo "$Person1 :: Don't want to brag it but, I think no one has stuck in my head like you did and I like it for some reason."
+echo "$Person1 :: pora puski."
