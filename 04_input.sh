ca387b8690a894c866628f528f8a2680482e660f
sivaanil-lab <timmisettyshiva@gmail.com>
2026-03-12 00:12:45 +0530

    variables added

---
+++ 04_input.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+Person1=Shiva
+Person2=Sathi
+
+echo "$Person1 :: Hey, Do you have a minute!"
+echo "$Person2 :: Hi $Person1, Yeah Wssup?"
+echo "$Person1 :: Please enter your password"
+read PASS
+echo "$Person2 :: My password is $PASS"
